fde89de5df96fc5a5e8afe30191e594a3cad46ce
Alex Matsuev <alex.matsuev@gmail.com>
2022-11-22 04:01:22 +0700

    Update tests

--- internal/router/router_test.go
+++ internal/router/router_test.go
@@ -12,21 +12,65 @@ func Test_handleGetHello(t *testing.T) {
 	testCases := []struct {
 		name   string
 		code   int
+		mode   string
 		result string
 	}{
 		{
-			name:   "valid",
+			name:   "default valid",
+			mode:   "default",
+			code:   http.StatusOK,
+			result: "Hello!",
+		},
+		{
+			name:   "gin valid",
+			mode:   "gin",
 			code:   http.StatusOK,
 			result: "Hello!",
 		},
 	}
 
-	r := setupRouter("default")
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := setupRouter(tc.mode)
+
+			rec := httptest.NewRecorder()
+			req, _ := http.NewRequest(http.MethodGet, "/hello", nil)
+
+			r.ServeHTTP(rec, req)
+
+			assert.Equal(t, tc.code, rec.Code)
+			assert.Equal(t, tc.result, rec.Body.String())
+		})
+	}
+}
+
+func Test_handlePostHello(t *testing.T) {
+	testCases := []struct {
+		name   string
+		mode   string
+		code   int
+		result string
+	}{
+		{
+			name:   "default valid",
+			mode:   "default",
+			code:   http.StatusOK,
+			result: "Hello! post request",
+		},
+		{
+			name:   "gin valid",
+			mode:   "gin",
+			code:   http.StatusOK,
+			result: "Hello! post request",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
+			r := setupRouter(tc.mode)
+
 			rec := httptest.NewRecorder()
-			req, _ := http.NewRequest(http.MethodGet, "/hello", nil)
+			req, _ := http.NewRequest(http.MethodPost, "/hello", nil)
 
 			r.ServeHTTP(rec, req)
 
